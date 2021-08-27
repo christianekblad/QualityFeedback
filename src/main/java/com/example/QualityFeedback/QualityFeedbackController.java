b05a9e19b9284658cf21783cb7ecb5d476622a02
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-08-27 14:45:28 +0200

    added filtering on product number

--- src/main/java/com/example/QualityFeedback/QualityFeedbackController.java
+++ src/main/java/com/example/QualityFeedback/QualityFeedbackController.java
@@ -3,10 +3,7 @@ package com.example.QualityFeedback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -41,8 +38,8 @@ public class QualityFeedbackController {
     }
 
     @GetMapping("/feedbacksByProductID")
-    public String feedbacksByProductID(Model model) {
-        List<Feedback> feedbacks = (List<Feedback>) feedbackRepository.findByproductNumberEquals(123);
+    public String feedbacksByProductID(Model model, @RequestParam Integer productNumber) {
+        List<Feedback> feedbacks = (List<Feedback>) feedbackRepository.findByproductNumberEquals(productNumber);
         model.addAttribute("feedbacks", feedbacks);
         return "feedbacks";
     }
